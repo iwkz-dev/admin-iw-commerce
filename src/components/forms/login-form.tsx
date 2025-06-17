d2d7bf7bee2b16bf20fa686829181c6ad6bc9074
faisalprady13 <faisalprady@gmail.com>
2025-06-17 09:10:13 +0200

    clean up login

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -7,36 +7,43 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Image from 'next/image';
 import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
+import shopImage from '../../../public/shop.jpg';
+import { Spinner } from '../ui/spinner';
 
 export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) {
-  const [errors] = useState<{ email?: string; password?: string }>({}); // TODO: handle validation errors
-  // const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+  const [error, setError] = useState<string | null>('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/';
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     const formData = new FormData(e.currentTarget);
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
 
-    // const newErrors: typeof errors = {};
-    // if (!email.includes('@')) newErrors.email = 'Invalid email address';
-    // if (password.length < 6) newErrors.password = 'Password must be at least 6 characters';
-
-    // if (Object.keys(newErrors).length > 0) {
-    //   setErrors(newErrors);
-    //   return;
-    // }
-
-    await signIn('credentials', {
+    const res = await signIn('credentials', {
       email,
       password,
-      redirect: true,
+      redirect: false,
       callbackUrl,
     });
+
+    if (res?.error) {
+      setError('Invalid email or password');
+      setLoading(false);
+    } else if (res?.ok) {
+      setError(null);
+      router.push(res.url || '/'); // Use callbackUrl here if stored
+      setLoading(false);
+    } else {
+      setError('Invalid email or password');
+      setLoading(false);
+    }
   };
 
   return (
@@ -54,26 +61,25 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
                 <Input name="email" id="email" type="email" placeholder="m@example.com" required />
-                {errors.email && <p className="text-sm text-red-500">{errors.email}</p>}
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
+                  {/* <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
                     Forgot your password?
-                  </a>
+                  </a> */}
                 </div>
                 <Input name="password" id="password" type="password" required />
-                {errors.password && <p className="text-sm text-red-500">{errors.password}</p>}
+                {error && <span className="text-destructive text-sm">{error}</span>}
               </div>
               <Button type="submit" className="w-full">
-                Login
+                {loading ? <Spinner className="text-white" /> : 'Login'}
               </Button>
             </div>
           </form>
           <div className="bg-muted relative hidden md:block">
             <Image
-              src="/login_hero.jpg"
+              src={shopImage}
               alt="Image"
               fill
               priority
